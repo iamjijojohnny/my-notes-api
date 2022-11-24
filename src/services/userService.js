0f657eb88f950abde7325aed54c3090d73c12592
Jijo Johny <orange.jijo@gmail.com>
2022-11-24 14:02:15 +0530

    initial commit

---
+++ src/services/userService.js
@@ -0,0 +1,22 @@
+async function signUp(fullname, email, ) {
+
+}
+
+async function login() {
+
+}
+
+async function update() {
+
+}
+
+async function remove() {
+
+}
+
+module.exports = {
+    signUp,
+    login,
+    update,
+    remove
+}
